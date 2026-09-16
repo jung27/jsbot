d26d91b0c19d300825c21f47582e27ad4809ceff
jung27 <junghhh4@gmail.com>
2026-09-17 05:34:57 +0900

    📝 소개 수정

--- commands/fun/너뭐냐.js
+++ commands/fun/너뭐냐.js
@@ -9,7 +9,7 @@ module.exports = {
     ),
   async execute(interaction) {
     await interaction.reply(
-      "안녕 나를 소개하지, 이름 정재선 직업은 developer\n취미는 taichi, meditation, 독서, 영화 시청",
+      "안녕 나를 소개하지, 이름 정재선 직업은 developer\n취미는 taichi, meditation, 독서, 영화 시청\n개발 해 탁 타닥 너 그리고 날 위해\n버그는 빼는 편이야 코드에서 질리는 맛이기에",
     );
   },
 };
