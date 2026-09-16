1d5a9425930b2cefa69dd89abe209c5d4e5642a9
jung27 <junghhh4@gmail.com>
2026-09-17 05:29:30 +0900

    ✨ 커맨드 "너뭐냐" 추가

---
+++ commands/fun/너뭐냐.js
@@ -0,0 +1,15 @@
+const { SlashCommandBuilder } = require("discord.js");
+const schedule = require("node-schedule");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("너뭐냐")
+    .setDescription(
+      "내가 뭐냐고? 내가 뭐냐고? 내가 뭐냐고? 내가 뭐냐고? 내가 뭐냐고? 내가 뭐냐고?",
+    ),
+  async execute(interaction) {
+    await interaction.reply(
+      "안녕 나를 소개하지, 이름 정재선 직업은 developer\n취미는 taichi, meditation, 독서, 영화 시청",
+    );
+  },
+};
